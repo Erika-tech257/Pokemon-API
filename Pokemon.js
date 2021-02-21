06410840ce1d8f6c7a1852b8479d4b9074e72066
ERika-tech257 <ErikaJtech@gmail.com>
2021-02-20 19:22:18 -0500

    added new pikachu image and weight

--- Pokemon.js
+++ Pokemon.js
@@ -5,16 +5,24 @@ const card = document.querySelector('.card');
 const cardBody = document.querySelector('.card-body');
 const goSomewhereBtn = document.querySelector('.btn');
 
-
 fetch(baseUrl)
-.then(response => response.json())
-.then(json => displayPokemon(json));
+    .then(response => response.json())
+    .then(json => displayPokemon(json));
 
 function displayPokemon(data) {
     console.log(data);
 
     // let img = document.createElement('img')
     // img.className = 'card-img-top';
+    // img.innerHTML = data.sprites.back_default;
+    let img = document.createElement('img')
+    img.setAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg");
+    img.setAttribute("width", "304");
+    img.setAttribute("height", "228");
+    img.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(img);
+
+    cardBody.insertBefore(img, goSomewhereBtn);
 
     let title = document.createElement('h2');
     title.className = 'card-title';
@@ -28,10 +36,76 @@ function displayPokemon(data) {
     types.innerText = data.types[0].type.name;
     types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
 
-    cardBody.insertBefore(types,goSomewhereBtn);
+    cardBody.insertBefore(types, goSomewhereBtn);
 
-} 
+    
+    let weight = document.createElement('p')
+    weight.className='card-body';
+    weight.innerText = data.weight;
+    types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
+    // document.body.appendChild(mweight)
+    
+    cardBody.insertBefore(weight,goSomewhereBtn);
+}
+  
+
+    /* Second Card **********/
+
+// const baseUrl = 'https://pokeapi.co/api/v2/pokemon/26';
+
+// const card = document.querySelector('.card1');
+// const cardBody = document.querySelector('.card-body1');
+// const goSomewhereBtn = document.querySelector('.btn1');
+
+// fetch(baseUrl)
+//     .then(response => response.json())
+//     .then(json => displayPokemon(json));
+
+// function displayPokemon(data) {
+//     console.log(data);
+
+//     // let img = document.createElement('img')
+//     // img.className = 'card-img-top';
+//     // img.innerHTML = data.sprites.back_default;
+//     let img = document.createElement('img')
+//     img.setAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/26.svg");
+//     img.setAttribute("width", "304");
+//     img.setAttribute("height", "228");
+//     img.setAttribute("alt", "Pokemon Image");
+//     document.body.appendChild(img);
+
+//     cardBody1.insertBefore(img, goSomewhereBtn);
+
+//     let title = document.createElement('h2');
+//     title.className = 'card-title';
+//     title.innerText = data.name;
+//     title.style = 'font-family: Permanent Marker, cursive; text-transform: uppercase; font-weight: bold;'
+
+//     cardBody1.insertBefore(title, goSomewhereBtn);
+
+//     let types = document.createElement('p')
+//     types.className = 'card-text';
+//     types.innerText = data.types[0].type.name;
+//     types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
+
+//     cardBody1.insertBefore(types, goSomewhereBtn);
+
+    
+//     let weight = document.createElement('p')
+//     weight.className='card-body';
+//     weight.innerText = data.weight;
+//     types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
+//     // document.body.appendChild(mweight)
+    
+//     cardBody1.insertBefore(weight,goSomewhereBtn);
+// }
+//     cardBody()
+    
+    
+
+
 let themeSong = document.getElementById('myAudio');
 
 function playAudio() {
-    themeSong.play();}
+    themeSong.play();
+}
