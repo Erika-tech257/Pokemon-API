9236f7c1fdbd6bed58859bbd97cec611956691ac
ERika-tech257 <ErikaJtech@gmail.com>
2021-02-22 17:05:30 -0500

    updated pokemon card, added weight

--- Pokemon.js
+++ Pokemon.js
@@ -38,70 +38,23 @@ function displayPokemon(data) {
 
     cardBody.insertBefore(types, goSomewhereBtn);
 
-    
+
     let weight = document.createElement('p')
-    weight.className='card-body';
+    weight.className = 'card-body';
     weight.innerText = data.weight;
     types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
     // document.body.appendChild(mweight)
-    
-    cardBody.insertBefore(weight,goSomewhereBtn);
+
+    cardBody.insertBefore(weight, goSomewhereBtn);
 }
-  
+cardBody();
 
-    /* Second Card **********/
+/* Second Card **********/
 
-// const baseUrl = 'https://pokeapi.co/api/v2/pokemon/26';
 
-// const card = document.querySelector('.card1');
-// const cardBody = document.querySelector('.card-body1');
-// const goSomewhereBtn = document.querySelector('.btn1');
 
-// fetch(baseUrl)
-//     .then(response => response.json())
-//     .then(json => displayPokemon(json));
 
-// function displayPokemon(data) {
-//     console.log(data);
 
-//     // let img = document.createElement('img')
-//     // img.className = 'card-img-top';
-//     // img.innerHTML = data.sprites.back_default;
-//     let img = document.createElement('img')
-//     img.setAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/26.svg");
-//     img.setAttribute("width", "304");
-//     img.setAttribute("height", "228");
-//     img.setAttribute("alt", "Pokemon Image");
-//     document.body.appendChild(img);
-
-//     cardBody1.insertBefore(img, goSomewhereBtn);
-
-//     let title = document.createElement('h2');
-//     title.className = 'card-title';
-//     title.innerText = data.name;
-//     title.style = 'font-family: Permanent Marker, cursive; text-transform: uppercase; font-weight: bold;'
-
-//     cardBody1.insertBefore(title, goSomewhereBtn);
-
-//     let types = document.createElement('p')
-//     types.className = 'card-text';
-//     types.innerText = data.types[0].type.name;
-//     types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
-
-//     cardBody1.insertBefore(types, goSomewhereBtn);
-
-    
-//     let weight = document.createElement('p')
-//     weight.className='card-body';
-//     weight.innerText = data.weight;
-//     types.style = 'font-family: Permanent Marker, cursive; font-weight: bold; text-transform: uppercase;'
-//     // document.body.appendChild(mweight)
-    
-//     cardBody1.insertBefore(weight,goSomewhereBtn);
-// }
-//     cardBody()
-    
-    
 
 
 let themeSong = document.getElementById('myAudio');
